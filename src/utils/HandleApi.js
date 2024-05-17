5fa6ecaaf092f76f6e1dd75f69d6269be576a91c
Plyhub <93124646+ppppppppply@users.noreply.github.com>
2024-05-17 18:09:50 +0700

    Update HandleApi.js

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000";
+const baseUrl = "https://backendstudentlist.onrender.com";
 
 // Function to GET all ToDo items
 const GET = (setStudent) => {
